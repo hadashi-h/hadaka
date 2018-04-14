b7bc7d0ae10f08ff3e15909c11c08cfc586b5388
hadashi <hadashi.heitai@gmail.com>
2018-04-14 09:42:04 +0200

    Melody: added Log in screen. Removed unused usings

---
+++ LockScreen/Login.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LockScreen
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
