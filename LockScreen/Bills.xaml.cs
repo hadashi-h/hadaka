4caf0c0ab5439b133c077f596c70b8d791521dc4
hadashi <hadashi.heitai@gmail.com>
2018-04-14 13:02:25 +0200

    Melody: added bills window

---
+++ LockScreen/Bills.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows; 
+
+namespace LockScreen
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Bills.xaml
+    /// </summary>
+    public partial class Bills : Window
+    {
+        public Bills()
+        {
+            InitializeComponent();
+        }
+    }
+}
