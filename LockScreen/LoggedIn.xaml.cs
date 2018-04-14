4531f261c2ed408a31d00ed2f7a7236036947890
hadashi <hadashi.heitai@gmail.com>
2018-04-14 11:16:30 +0200

    Dashboard: added logged in window, some code refactoring

---
+++ LockScreen/LoggedIn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace LockScreen
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoggedIn.xaml
+    /// </summary>
+    public partial class LoggedIn : Window
+    {
+        public LoggedIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
