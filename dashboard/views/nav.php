921f2228e01efbb371b28f2cc1caf6b7ede72671
hadashi <hadashi.heitai@gmail.com>
2018-04-14 15:49:36 +0200

    Dashboard: fixed nav links

--- dashboard/views/nav.php
+++ dashboard/views/nav.php
@@ -1,7 +1,7 @@
 <div id="menu">
   <ul>
-    <a href="./"><li><i class="fas fa-home"></i><span>Dashboard</span></li></a>
-    <a href="sites/money"><li> <span>Bank account</span><i class="fas fa-piggy-bank"></i></li></a>
+    <a href="<?php echo Flight::routeUrl('/'); ?>"><li><i class="fas fa-home"></i><span>Dashboard</span></li></a>
+    <a href="money"><li> <span>Bank account</span><i class="fas fa-piggy-bank"></i></li></a>
     <a href="activity"><li><span>Activity</span><i class="fas fa-heartbeat"></i></li></a>
     <a href="computer"><li><span>Computer Security</span><i class="fas fa-desktop"></i></li></a>
     <a href="settings"><li><span>Settings</span><i class="fas fa-sliders-h"></i></li></a>
